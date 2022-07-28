15be45f7670fdad41075563f64ba3ad1bb0ee927
Minhntq19 <102934112+Minhntq19@users.noreply.github.com>
2022-07-28 21:21:18 +0700

    second commit

--- app.js
+++ app.js
@@ -1,73 +1,97 @@
-const btns = document.querySelector('button');
-const taskinput = document.querySelector('.input__text');
-const listtask = document.querySelector('.list');
-const html = [];
+const btnS = document.querySelector('button');
+const taskInput = document.querySelector('.input__text');
+const list = document.querySelector('.list');
 //render
 const app = {
-	render: () => {
-		const htmls = html.map((value, index) => {
-			return `<li class="list__task" id="${index}">
-				<input type="checkbox" class="check__box"/>
-				<div class="task__content" >${value}</div>
-				<i class="fa-solid fa-pen-to-square"></i>
-				<i class="fa-solid fa-trash-can"></i>
-			</li>`;
-		});
-		listtask.innerHTML = htmls.join('');
-		taskinput.value = '';
+	createTodo: (value) => {
+		const newLi = document.createElement('li');
+		newLi.classList.add('list__task');
+		const checkbox = document.createElement('input');
+		checkbox.type = 'checkbox';
+		checkbox.classList.add('check__box', 'checkmark');
+		const newDiv = document.createElement('div');
+		newDiv.classList.add('task__content');
+		newDiv.innerText = value;
+		const newEdit = document.createElement('i');
+		newEdit.classList.add('fa-solid', 'fa-pen-to-square');
+		const newDelete = document.createElement('i');
+		newDelete.classList.add('fa-solid', 'fa-trash-can');
+		newLi.append(checkbox, newDiv, newEdit, newDelete);
+		list.appendChild(newLi);
 	},
-	add: function () {
-		btns.onclick = () => {
-			if (taskinput.value) {
-				html.push(taskinput.value);
-				app.render();
+	add: () => {
+		//click add
+		btnS.onclick = () => {
+			if (taskInput.value) {
+				app.createTodo(taskInput.value);
+				taskInput.value = '';
 			}
 		};
-		taskinput.addEventListener('keypress', function (e) {
+		taskInput.addEventListener('keypress', function (e) {
 			if (e.key === 'Enter') {
 				e.preventDefault();
-				btns.click();
-				app.render();
+				btnS.click();
 			}
 		});
 	},
 	// Mark as done
 	mark: () => {
-		listtask.onclick = (e) => {
+		list.onclick = (e) => {
 			const parents = e.target.closest('li');
 			const child1 = e.target.closest('.fa-trash-can');
 			const child2 = e.target.closest('.check__box');
 			if (child2) {
 				if (child2.checked) {
-					parents.querySelector('.task__content').classList.add('done');
+					parents.classList.add('done');
 				} else {
-					parents.querySelector('.task__content').classList.remove('done');
+					parents.classList.remove('done');
 				}
 			}
 			if (child1) {
 				if (child1) {
 					parents.remove();
-					html.splice(parents.id, 1);
 				}
-				app.render();
 			}
-			console.log(child2, parents, child1);
 		};
 	},
-	// Delete task
-	// deletefnc: () => {
-	// 	listtask.onclick = (e) => {
-	// 		const parents = e.target.closest('li');
-	// 		const child1 = e.target.closest('.fa-trash-can');
-	//
-	// 		app.render();
-	// 	};
-	// },
+	showDone: () => {
+		const showBtn = document.querySelector('.show__done');
+		showBtn.onclick = () => {
+			app.hidePending();
+		};
+	},
+	showPending: () => {
+		const hideBtn = document.querySelector('.show__pending');
+		hideBtn.onclick = () => {
+			app.hideDone();
+		};
+	},
+	hideDone: () => {
+		const listTask = document.querySelectorAll('.done');
+		listTask.forEach((value) => {
+			value.classList.toggle('hide');
+		});
+	},
+	hidePending: () => {
+		const listTasks = document.querySelectorAll('.list__task');
+		const listPending = [];
+		for (var i = 0; i < listTasks.length; i++) {
+			if (listTasks[i].className !== 'list__task done') {
+				listPending.push(listTasks[i]);
+			}
+		}
+		listPending.forEach((e) => {
+			e.classList.toggle('hide');
+		});
+	},
+	edit: () => {},
+	//call events
 	start: () => {
-		app.render();
 		app.mark();
-		// app.deletefnc();
 		app.add();
+		app.showDone();
+		app.showPending();
+		app.edit();
 	},
 };
 app.start();
