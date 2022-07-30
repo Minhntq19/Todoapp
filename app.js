92a5607286c293a51d5e6bc1c6c2c6343b0bf2ed
Minhntq19 <102934112+Minhntq19@users.noreply.github.com>
2022-07-30 12:32:40 +0700

    third commit

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
+'use strict';
 const btnS = document.querySelector('button');
 const taskInput = document.querySelector('.input__text');
 const list = document.querySelector('.list');
+let i = 0;
 //render
 const app = {
+	checker: false,
 	createTodo: (value) => {
+		i = ++i;
 		const newLi = document.createElement('li');
-		newLi.classList.add('list__task');
+		if (app.checker) {
+			newLi.classList.add('list__task', 'hide');
+		} else {
+			newLi.classList.add('list__task');
+		}
 		const checkbox = document.createElement('input');
 		checkbox.type = 'checkbox';
-		checkbox.classList.add('check__box', 'checkmark');
-		const newDiv = document.createElement('div');
+		checkbox.setAttribute('id', `${i}`);
+		checkbox.classList.add('check__box');
+		const newDiv = document.createElement('label');
 		newDiv.classList.add('task__content');
+		newDiv.setAttribute('for', `${i}`);
 		newDiv.innerText = value;
 		const newEdit = document.createElement('i');
 		newEdit.classList.add('fa-solid', 'fa-pen-to-square');
@@ -18,12 +28,14 @@ const app = {
 		newDelete.classList.add('fa-solid', 'fa-trash-can');
 		newLi.append(checkbox, newDiv, newEdit, newDelete);
 		list.appendChild(newLi);
+		console.log(app.checker);
 	},
 	add: () => {
 		//click add
 		btnS.onclick = () => {
 			if (taskInput.value) {
 				app.createTodo(taskInput.value);
+				app.saveLocal(taskInput.value);
 				taskInput.value = '';
 			}
 		};
@@ -40,6 +52,7 @@ const app = {
 			const parents = e.target.closest('li');
 			const child1 = e.target.closest('.fa-trash-can');
 			const child2 = e.target.closest('.check__box');
+			const child3 = parents.children;
 			if (child2) {
 				if (child2.checked) {
 					parents.classList.add('done');
@@ -49,15 +62,21 @@ const app = {
 			}
 			if (child1) {
 				if (child1) {
-					parents.remove();
+					parents.classList.add('fade');
+					setTimeout(() => {
+						parents.remove();
+					}, 200);
+					app.deleteLocal(child3[1].textContent);
 				}
 			}
 		};
 	},
 	showDone: () => {
 		const showBtn = document.querySelector('.show__done');
-		showBtn.onclick = () => {
+		showBtn.onclick = (e) => {
 			app.hidePending();
+			const showChild = e.target.closest('input');
+			app.checker = showChild.checked;
 		};
 	},
 	showPending: () => {
@@ -69,7 +88,10 @@ const app = {
 	hideDone: () => {
 		const listTask = document.querySelectorAll('.done');
 		listTask.forEach((value) => {
-			value.classList.toggle('hide');
+			value.classList.toggle('fade');
+			setTimeout((e) => {
+				value.classList.toggle('hide');
+			}, 300);
 		});
 	},
 	hidePending: () => {
@@ -80,11 +102,45 @@ const app = {
 				listPending.push(listTasks[i]);
 			}
 		}
-		listPending.forEach((e) => {
-			e.classList.toggle('hide');
+		listPending.forEach((value) => {
+			value.classList.toggle('fade');
+			setTimeout((e) => {
+				value.classList.toggle('hide');
+			}, 300);
 		});
 	},
 	edit: () => {},
+	saveLocal: (todo) => {
+		let todos;
+		if (localStorage.getItem('todos')) {
+			todos = JSON.parse(localStorage.getItem('todos'));
+		} else {
+			todos = [];
+		}
+		todos.push(todo);
+		localStorage.setItem('todos', JSON.stringify(todos));
+	},
+	loadLocal: () => {
+		let todos;
+		if (localStorage.getItem('todos')) {
+			todos = JSON.parse(localStorage.getItem('todos'));
+		} else {
+			todos = [];
+		}
+		todos.forEach((value) => {
+			app.createTodo(value);
+		});
+	},
+	deleteLocal: (value) => {
+		let todos;
+		if (localStorage.getItem('todos')) {
+			todos = JSON.parse(localStorage.getItem('todos'));
+		} else {
+			todos = [];
+		}
+		todos.splice(todos.indexOf(value), 1);
+		localStorage.setItem('todos', JSON.stringify(todos));
+	},
 	//call events
 	start: () => {
 		app.mark();
@@ -92,6 +148,7 @@ const app = {
 		app.showDone();
 		app.showPending();
 		app.edit();
+		app.loadLocal();
 	},
 };
 app.start();
